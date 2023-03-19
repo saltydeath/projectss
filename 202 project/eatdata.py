36afd56aa7bc779829eee9b7cc823dcfc4c5b52c
saltydeath <kwekzhiyi2511@gmail.com>
2023-03-19 18:07:31 +0800

    more progress

--- 202 project/eatdata.py
+++ 202 project/eatdata.py
@@ -1,4 +1,12 @@
 from bs4 import BeautifulSoup
+#make array of distancess
+def distance_array(arr, n):
+    dist = [[0 for x in range(n)] for y in range(n)] 
+    print(dist)
+    
+    return 0
+
+#THIS IS MAIN, READS XML
 # Reading the data inside the xml
 # file to a variable under the name
 # data
@@ -9,14 +17,14 @@ with open('n030-k03.xml', 'r') as f:
 # the beautifulsoup parser, storing
 # the returned object
 Bs_data = BeautifulSoup(data, "xml")
- 
+n = len(Bs_data.find_all('node'))
 # Finding all instances of tag
 # `node`
 # b_node = Bs_data.find_all('node')
 
 # print(b_node)
-node_table = [None]*30
-for i in range(1, 31):
+node_table = [None]*n
+for i in range(1, n+1):
     node = Bs_data.find('node', {'id': i})
     id = node.get('id')
     x = node.find('cx').text
@@ -28,5 +36,5 @@ for i in range(1, 31):
     else:
         node_table[i-1] = (id, x, y, None)
     
-    
-    print(node_table[i-1])
+    #print(node_table[i-1])
+distance_array(node_table, n)

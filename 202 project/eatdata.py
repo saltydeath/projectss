7fa46caeca4c74cee2642b729cd44b115af68e50
saltydeath <kwekzhiyi2511@gmail.com>
2023-03-19 17:45:12 +0800

    we have stored the xml data in a table :D

--- 202 project/eatdata.py
+++ 202 project/eatdata.py
@@ -1,5 +1,4 @@
 from bs4 import BeautifulSoup
- 
 # Reading the data inside the xml
 # file to a variable under the name
 # data
@@ -12,7 +11,22 @@ with open('n030-k03.xml', 'r') as f:
 Bs_data = BeautifulSoup(data, "xml")
  
 # Finding all instances of tag
-# `unique`
-b_node = Bs_data.find_all('node')
- 
-print(b_node)
+# `node`
+# b_node = Bs_data.find_all('node')
+
+# print(b_node)
+node_table = [None]*30
+for i in range(1, 31):
+    node = Bs_data.find('node', {'id': i})
+    id = node.get('id')
+    x = node.find('cx').text
+    y = node.find('cy').text
+    if i > 1:
+        request = Bs_data.find('request', {'node': i})
+        weight = request.find('quantity').text
+        node_table[i-1] = (id, x, y, weight)
+    else:
+        node_table[i-1] = (id, x, y, None)
+    
+    
+    print(node_table[i-1])

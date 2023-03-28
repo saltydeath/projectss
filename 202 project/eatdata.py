2bf968544e4cf7c36053a56fc1148e3e6a4b6215
saltydeath <kwekzhiyi2511@gmail.com>
2023-03-28 14:12:21 +0800

    eatdata now eats the 101 data file

--- 202 project/eatdata.py
+++ 202 project/eatdata.py
@@ -16,7 +16,7 @@ def distance_array(arr, n):
 
 #THIS IS MAIN, READS XML
 # Reading the data inside the xml
-with open('n030-k03.xml', 'r') as f:
+with open('n101-k14.xml', 'r') as f:
     data = f.read()
  
 # Passing the stored data inside the beautifulsoup parser, storing the returned object
